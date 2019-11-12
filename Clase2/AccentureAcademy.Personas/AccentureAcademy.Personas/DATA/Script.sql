dd38e49993b83c68fcfd62e5c981a9b76f21ae50
DrakoZelda <drako.cjs@hotmail.com>
2019-11-12 16:36:22 -0300

    Clase 2 incompleta

---
+++ Clase2/AccentureAcademy.Personas/AccentureAcademy.Personas/DATA/Script.sql
@@ -0,0 +1,41 @@
+﻿CREATE DATABASE [AccentureAcademyWorks.Sql]
+GO
+
+use [AccentureAcademyWorks.Sql]
+GO
+
+CREATE TABLE Persona
+(
+	id int primary key identity(1,1),
+	nombre varchar(50) not null,
+	apellido varchar(50) not null,
+	fechaNacimiento datetime not null,
+	CONSTRAINT UQ_NOMBRE_APELLIDO UNIQUE (nombre, apellido)
+);
+
+INSERT INTO Persona
+(nombre, apellido, fechaNacimiento)
+VALUES
+('Juan', 'Topo', CURRENT_TIMESTAMP),
+('jhin', 'Khada', GETDATE()),
+('Max', 'Power', '1980-11-10'),
+('Aquiles', 'Baeza', DATEFROMPARTS(1990,3,2)),
+('Ted', 'Tonks', DATEADD(YEAR, -10, GETDATE()))
+
+
+UPDATE Persona
+SET fechaNacimiento = DATEFROMPARTS(1995, 9, 14)
+WHERE nombre = 'Max'
+
+UPDATE Persona
+SET fechaNacimiento = DATEADD(YEAR, -10, fechaNacimiento)
+WHERE nombre = 'jhin'
+
+/*
+-- el query falla debido al constraint impuesto. (uq_nombre_apellido)
+
+INSERT INTO Persona
+(nombre, apellido, fechaNacimiento)
+VALUES
+('Max', 'Power', GETDATE())
+*/
---
+++ Clase2/AccentureAcademy.Personas/AccentureAcademy.Personas/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace AccentureAcademy.Personas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Console.WriteLine("Ingrese su nombre");
+            string nombreIngresado = Console.ReadLine();
+            Console.WriteLine("Ingrese su apellido");
+            string apellidoIngresado = Console.ReadLine();
+            Console.WriteLine("Ingrese su fecha de nacimiento");
+            //string fechaNacimientoIngresada = Console.ReadLine();
+            Console.WriteLine("Año:");
+            Int32 anioIngresado = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mes:");
+            Int32 mesIngresado = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dia:");
+            Int32 diaIngresado = Convert.ToInt32(Console.ReadLine());
+            */
+            SqlConnection cnn = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=AccentureAcademyWorks.Sql;Integrated Security=True");
+            cnn.Open();
+
+            try
+            {
+                SqlCommand sql = new SqlCommand();
+                sql.Connection = cnn;
+               /* sql.CommandText = "insert into Persona(nombre, apellido, fechaNacimiento) values(@nombre, @apellido, @fechaNacimiento)";
+                sql.Parameters.Add(new SqlParameter("nombre", nombreIngresado));
+                sql.Parameters.Add(new SqlParameter("apellido", apellidoIngresado));
+                //sql.Parameters.Add(new SqlParameter("fechaNacimiento", Convert.ToDateTime(fechaNacimientoIngresada)));
+                sql.Parameters.Add(new SqlParameter("fechaNacimiento", new DateTime(anioIngresado, mesIngresado, diaIngresado)));
+                sql.ExecuteNonQuery(); */
+                sql.CommandText = "select * from Persona";
+                SqlDataReader reader = sql.ExecuteReader();
+                foreach (DbDataRecord registro in reader)
+                {
+                    string id = Convert.ToString(registro.GetInt32(registro.GetOrdinal("id")));
+                    string nombre = registro.GetString(registro.GetOrdinal("nombre"));
+                    string apellido = registro.GetString(registro.GetOrdinal("apellido"));
+                    string fechaNacimiento = Convert.ToString(registro.GetDateTime(registro.GetOrdinal("fechaNacimiento")));
+                    Console.WriteLine($"Id: {id}, Nombre: {nombre} Apellido: {apellido}, fecha de nacimiento: {fechaNacimiento}");
+
+                }
+                Console.WriteLine("Ingrese el id de la persona a la cual desee eliminar");
+                int idEliminar = Convert.ToInt32(Console.ReadLine());
+                sql.Connection.Close();
+                sql.Connection.Open();
+                sql.CommandText = "delete from Persona where id = @idEliminar";
+                sql.Parameters.Add(new SqlParameter("idEliminar", idEliminar));
+                sql.ExecuteNonQuery();
+            }
+            finally
+            {
+                cnn.Close();
+            }
+            
+
+
+        }
+    }
+}
