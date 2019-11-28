832d1e4ac54dd7d6b8d682f9cbd052aa365325d2
DrakoZelda <drako.cjs@hotmail.com>
2019-11-28 17:39:35 -0300

    clase 13

---
+++ Clase13/AccentureAcademy.Clase13.Dudas/AccentureAcademy.Clase13.Dudas/Controllers/LibroController.cs
@@ -0,0 +1,50 @@
+﻿using AccentureAcademy.Clase13.Dudas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccentureAcademy.Clase13.Dudas.Controllers
+{
+    public class LibroController : Controller
+    {
+        private AccentureAcademyBookStoreEntities db = new AccentureAcademyBookStoreEntities();
+
+        public ActionResult Agregar()
+        {
+            Book libro = new Book();
+            return View("Editar", libro);
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(Book book, int autores)
+        {
+            WrittenBy escritoPor = new WrittenBy();
+            //escritorPor.Book = book
+            escritoPor.Autor = db.Autor.Find(autores);
+            book.WrittenBy.Add(escritoPor);
+
+            db.Book.Add(book);
+            db.SaveChanges();
+
+            return Content("Libro creado satisfactoriamente.");
+        }
+        
+        public ActionResult Editar(int id)
+        {
+            Book libro = db.Book.Find(id);
+            return View(libro);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Book libro)
+        {
+            db.Book.Attach(libro);
+            db.Entry(libro).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return Content("Libro editado satisfactoriamente.");
+        }
+    }
+}
